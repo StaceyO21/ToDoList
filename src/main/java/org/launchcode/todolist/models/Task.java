e4c28a7578a271eac866a2bfc228669f54dffddc
StaceyO21 <staceyobrien21@gmail.com>
2025-07-05 20:50:08 -0500

    changes

--- src/main/java/org/launchcode/todolist/models/Task.java
+++ src/main/java/org/launchcode/todolist/models/Task.java
@@ -1,19 +1,26 @@
 package org.launchcode.todolist.models;
 
 
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
-//@Table(name = "task")
-public class Task extends AbstractEntity{
+@Table(name = "Task")
+public class Task{
 
-    private String name;
+    @Id
+    @Generated(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String task;
 
     private LocalDate dateRegistered = LocalDate.now();
 
